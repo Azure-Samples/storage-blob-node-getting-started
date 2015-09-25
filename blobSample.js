ae06725ffd3b116c9b1614d67a70407385be1162
Yang Xia <yaxia@microsoft.com>
2015-09-25 17:02:43 +0800

    Add comments for the functions

--- blobSample.js
+++ blobSample.js
@@ -79,7 +79,12 @@ function runBlobSamples() {
   scenarios[current].scenario(callback);
 }
 
-// Block blob basics
+/**
+* Page blob basics.
+* @ignore
+* 
+* @param {errorOrResult}        callback                         The callback function.
+*/
 function basicStorageBlockBlobOperations(callback) {
   // Create a blob client for interacting with the blob service from connection string
   // How to create a storage connection string - http://msdn.microsoft.com/en-us/library/azure/ee758697.aspx
@@ -185,7 +190,12 @@ function basicStorageBlockBlobOperations(callback) {
   });
 }
 
-// Page blob basics
+/**
+* Page blob basics.
+* @ignore
+* 
+* @param {errorOrResult}        callback                         The callback function.
+*/
 function basicStoragePageBlobOperations(callback) {
   // Create a blob client for interacting with the blob service from connection string
   // How to create a storage connection string - http://msdn.microsoft.com/en-us/library/azure/ee758697.aspx
@@ -259,7 +269,32 @@ function basicStoragePageBlobOperations(callback) {
   });
 }
 
-function listBlobs (blobService, container, options, token, callback) {
+/**
+* Lists blobs in the container.
+* @ignore
+*
+* @param {BlobService}        blobService                         The blob service client.
+* @param {string}             container                           The container name.
+* @param {object}             token                               A continuation token returned by a previous listing operation. 
+*                                                                 Please use 'null' or 'undefined' if this is the first operation.
+* @param {object}             [options]                           The request options.
+* @param {int}                [options.maxResults]                Specifies the maximum number of directories to return per call to Azure ServiceClient. 
+*                                                                 This does NOT affect list size returned by this function. (maximum: 5000)
+* @param {LocationMode}       [options.locationMode]              Specifies the location mode used to decide which location the request should be sent to. 
+*                                                                 Please see StorageUtilities.LocationMode for the possible values.
+* @param {int}                [options.timeoutIntervalInMs]       The server timeout interval, in milliseconds, to use for the request.
+* @param {int}                [options.maximumExecutionTimeInMs]  The maximum execution time, in milliseconds, across all potential retries, to use when making this request.
+*                                                                 The maximum execution time interval begins at the time that the client begins building the request. The maximum
+*                                                                 execution time is checked intermittently while performing requests, and before executing retries.
+* @param {string}             [options.clientRequestId]           A string that represents the client request ID with a 1KB character limit.
+* @param {bool}               [options.useNagleAlgorithm]         Determines whether the Nagle algorithm is used; true to use the Nagle algorithm; otherwise, false.
+*                                                                 The default value is false.
+* @param {errorOrResult}      callback                            `error` will contain information
+*                                                                 if an error occurs; otherwise `result` will contain `entries` and `continuationToken`. 
+*                                                                 `entries`  gives a list of directories and the `continuationToken` is used for the next listing operation.
+*                                                                 `response` will contain information related to this operation.
+*/
+function listBlobs (blobService, container, token, options, callback) {
   var blobs = [];
   
   blobService.listBlobsSegmented(container, token, options, function(error, result) {
@@ -267,7 +302,7 @@ function listBlobs (blobService, container, options, token, callback) {
     var token = result.continuationToken;
     if (token) {
       console.log('   Received a page of results. There are ' + result.entries.length + ' blobs on this page.');
-      listBlobs(blobService, container, options, token, callback);
+      listBlobs(blobService, container, token, options, callback);
     } else {
       console.log('   Completed listing. There are ' + blobs.length + ' blobs.');
       callback(null, blobs);
@@ -275,15 +310,37 @@ function listBlobs (blobService, container, options, token, callback) {
   });
 }
 
+/**
+* Generates a random bytes of buffer.
+* @ignore
+*
+* @param {int}        size                         The size of the buffer in bytes.
+* @return {Buffer}
+*/
 function getRandomBuffer(size) {
   return crypto.randomBytes(size);
 }
 
+/**
+* Generates a random ID for the blob block.
+* @ignore
+*
+* @param {int}        index                        The index of the block.
+* @return {string}
+*/
 function getBlockId(index) {
   var prefix = zeroPaddingString(Math.random().toString(16), 8);
   return prefix + '-' + zeroPaddingString(index, 6);
 }
 
+/**
+* Adds paddings to a string.
+* @ignore
+*
+* @param {string}     str                          The input string.
+* @param {int}        len                          The length of the string.
+* @return {string}
+*/
 function zeroPaddingString(str, len) {
   var paddingStr = '0000000000' + str;
   if(paddingStr.length < len) {
@@ -293,6 +350,12 @@ function zeroPaddingString(str, len) {
   }
 }
 
+/**
+* Reads the configurations.
+* @ignore
+*
+* @return {Object}
+*/
 function readConfig() {
   var config = JSON.parse(fs.readFileSync('app.config', 'utf8'));
   if (config.useDevelopmentStorage) {
